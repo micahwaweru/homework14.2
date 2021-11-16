60b9169df603a50147579d5032677b32cfa60377
micahwaweru <micah.waweru@gmail.com>
2021-11-15 18:03:01 -0600

    init server js file

---
+++ server.js
@@ -0,0 +1,33 @@
+const express =require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+//const sequelize = require('')
+//const routes=require('')
+//const helpers = require('')
+
+
+const SequelizeStore=require('connect-session-sequelize')(session.Store);
+const sess={
+    secret:'My secret of secrets',
+    cookie:{},
+    resave:false,
+    saveUninitialized: false,
+};
+
+//app build from activity 15 - Sessions
+app.use(session(sess));
+const hbs=exphbs.create({helpers});
+app.engine('handlebars',hbs.engine);
+app.set('view engine','handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
+//app.use(routes)
+
+//init sequelize here
